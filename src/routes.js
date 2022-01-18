b9a6728cd95b45e1c0fde59dd13a98faf6f13e52
André Luiz Vicente <andre.vicente@zup.com.br>
2022-01-18 16:21:44 -0300

    feat: criar dashboard

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import PrivateRoute from './auth';
 import Login from "./components/pages/Login";
 import Dashboard from './components/pages/Dashboard';
+import Landing from './components/pages/Landing';
 
 const Routes = () => (
     <Router>
         <Switch>
             <Route exact path='/' component={Login}/>
             <PrivateRoute path='/dashboard' component={Dashboard}/>
+            <Route exact path='/landing' component={Landing}/>
         </Switch>
     </Router>
 
