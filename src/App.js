4a74a26ac8b0ae432b200a9fe628930720628cfe
André Luiz Vicente <andre.vicente@zup.com.br>
2022-01-18 14:46:09 -0300

    refactor: criar modelo de rotas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import react, {Component} from "react";
 import "./App.css"
-import Login from "./components/pages/Login";
+import Routes from "./routes";
 
 
 class App extends Component {
     render(){
         return(
             <div className="App">
-                <Login/>
+                <Routes/>
             </div>
         );
     }
